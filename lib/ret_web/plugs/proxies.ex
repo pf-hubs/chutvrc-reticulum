d05663d0648b568deb040f6a71c64db07114f8f1
YHhaoareyou <mlapenang1998@gmail.com>
2023-05-21 20:46:57 +0900

    feat: auto reload clients in room after updating room settings from admin page

--- lib/ret_web/plugs/proxies.ex
+++ lib/ret_web/plugs/proxies.ex
@@ -1,10 +1,17 @@
 defmodule RetWeb.Plugs.PostgrestProxy do
   use Plug.Builder
+  require Logger
 
   plug :call
 
   @spec call(Plug.Conn.t(), []) :: Plug.Conn.t()
   def call(conn, []) do
+    case conn.method() do
+      "PATCH" ->
+        on_patch(conn)
+      _ ->
+        conn
+    end
     opts = ReverseProxyPlug.init(upstream: "http://#{hostname()}:3001")
     ReverseProxyPlug.call(conn, opts)
   end
@@ -15,6 +22,19 @@ defmodule RetWeb.Plugs.PostgrestProxy do
       :ret
       |> Application.fetch_env!(__MODULE__)
       |> Keyword.fetch!(:hostname)
+
+  defp on_patch(conn) do
+    case Regex.run(~r/id=eq\.(\d+)/, conn.query_string) do
+      nil -> {:error, "No match found"}
+      match ->
+        id = List.last(match)
+        case Ret.Hub |> Ret.Repo.get_by(hub_id: id) do
+          nil -> {:error, "No record found"}
+          hub ->
+            RetWeb.Endpoint.broadcast("hub:" <> hub.hub_sid, "hub_refresh_by_admin", %{})
+        end
+    end
+  end
 end
 
 defmodule RetWeb.Plugs.ItaProxy do
