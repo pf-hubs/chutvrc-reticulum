5f1d58dec59b5f05732ae56668cea7c29c7157c2
YHhaoareyou <mlapenang1998@gmail.com>
2024-02-25 14:18:01 +0000

    feat: add bash file for dev environment

---
+++ scripts/run-dev.sh
@@ -0,0 +1,2 @@
+PERMS_KEY=$MOZILLA_RETICULUM_PERMS_PRIVATE_KEY
+HUBS_CLIENT_INTERNAL_HOSTNAME="change.vr.u-tokyo.ac.jp" HUBS_ADMIN_INTERNAL_HOSTNAME="change.vr.u-tokyo.ac.jp" SPOKE_INTERNAL_HOSTNAME="change.vr.u-tokyo.ac.jp" DIALOG_HOSTNAME="change.vr.u-tokyo.ac.jp" PERMS_KEY="$PERMS_KEY" iex -S mix phx.server
